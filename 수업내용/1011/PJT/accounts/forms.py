c15f9484917e76d21c061d17330639d268a39ffd
pangpyo <kkp0639@gmail.com>
2022-10-12 01:21:35 +0900

    1011 Django TIL

---
+++ 수업내용/1011/PJT/accounts/forms.py
@@ -0,0 +1,8 @@
+# accounts/forms.py
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
