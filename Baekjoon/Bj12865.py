7bb939dedda047fcb280d5a693b94662f27da735
pangpyo <kkp0639@gmail.com>
2022-11-23 17:23:32 +0900

    1123 PS

--- Baekjoon/Bj12865.py
+++ Baekjoon/Bj12865.py
@@ -4,23 +4,20 @@
 import sys
 
 
-sys.stdin = open('input.txt')
+sys.stdin = open("input.txt")
 
 N, K = map(int, input().split())
 items = []
-for i in range(N) :
+for i in range(N):
     items.append(tuple(map(int, input().split())))
 
-D = [[0]*(K+1) for _ in range(N+1)]
-for i in range(1, N+1) :
-    for j in range(1, K+1) :
-        W, V = items[i-1]
-        if j < W :
-            D[i][j] = D[i-1][j]
-        else :
-            D[i][j] = max(D[i-1][j-W] + V, D[i-1][j])
+D = [[0] * (K + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        W, V = items[i - 1]
+        if j < W:
+            D[i][j] = D[i - 1][j]
+        else:
+            D[i][j] = max(D[i - 1][j - W] + V, D[i - 1][j])
 
 print(D[-1][-1])
-
-
-
