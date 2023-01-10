87233e556c9c32b1f6d4c9337a93d10f7b0bc7fe
pangpyo <kkp0639@gmail.com>
2023-01-10 23:38:26 +0900

    0110 PS

--- Baekjoon/Bj11054.py
+++ Baekjoon/Bj11054.py
@@ -3,19 +3,19 @@
 N = int(input())
 
 A = list(map(int, input().split()))
-U = [1] * N
-D = [1] * N
+U = [1] * N # 증가하는 부분수열의 최대 길이
+D = [1] * N # 감소하는 부분수열의 최대 길이
 for i in range(N):
     for j in range(i):
-        if A[j] < A[i]:
+        if A[j] < A[i]: # 증가하는 부분은 처음부터 세어준다
             U[i] = U[j] + 1 if (U[j] + 1) > U[i] else U[i]
-        if A[-(j + 1)] < A[-(i + 1)]:
+        if A[-(j + 1)] < A[-(i + 1)]: # 감소하는부분은 가장 끝부터 세어준다
             D[-(i + 1)] = (
                 D[-(j + 1)] + 1 if (D[-(j + 1)] + 1) > D[-(i + 1)] else D[-(i + 1)]
             )
 
 ans = 0
-for i in range(N):
+for i in range(N): # 둘을 더한 값이 가장 높은것이 답
     ans = max(U[i] + D[i] - 1, ans)
 
 print(ans)
--- Baekjoon/Bj15686.py
+++ Baekjoon/Bj15686.py
@@ -1,29 +1,23 @@
 # 15686 치킨 배달 G5
 
 from itertools import combinations
-import sys
-
-input = sys.stdin.readline
 
 N, M = map(int, input().split())
 
-city = []
+
 chicken = []
 houses = []
 for i in range(N):
-    city.append(list(map(int, input().split())))
+    temp = list(map(int, input().split()))
     for j in range(N):
-        if city[i][j] == 2:
+        if temp[j] == 2:
             chicken.append((i, j))
-        elif city[i][j] == 1:
+        elif temp[j] == 1:
             houses.append((i, j))
 chickens = list(combinations(chicken, M))
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
 
-
-def chickendis(x, y, chicken):
+def chickendis(x, y, chicken):  # 가장 가까운 치킨집까지의 거리를 잰다
     ans = 2 * N
     for chi in chicken:
         cx, cy = chi
@@ -33,9 +27,9 @@ def chickendis(x, y, chicken):
 
 def solution():
     answer = 2 * N * len(houses)
-    for chi in chickens:
+    for chi in chickens:  #
         ans = 0
-        for house in houses:
+        for house in houses:  # 집별로 치킨거리를 구한다
             x, y = house
             ans += chickendis(x, y, chi)
 
