c528b6048cd7c9ab5fa00b76de5d404cc50f36be
Pangpyo <kkp0639@gmail.com>
2023-08-21 09:15:17 +0900

    0821

--- Baekjoon/Bj2285.py
+++ Baekjoon/Bj2285.py
@@ -1,26 +1,26 @@
-# 2258 정육점 G4
+# 2285 우체국 G4
 
 import sys
 
 
-def solution():
+def solution() :
     input = sys.stdin.readline
-    N, M = map(int, input().split())
-    meats = [[0, 0, 0]]
-    for i in range(N):
-        meat, cost = map(int, input().split())
-        meats.append([meat, cost, cost])
-    meats.sort(key=lambda x: (x[1], -x[0]))
-    for i in range(1, N + 1):
-        if meats[i][1] == meats[i - 1][1]:
-            meats[i][2] += meats[i - 1][2]
-        meats[i][0] += meats[i - 1][0]
-    meats.sort(key=lambda x: (x[2], -x[0]))
-    for meat, cost, tcost in meats:
-        if meat >= M:
-            return tcost
-    return -1
+    N = int(input())
+    XA = []
+    popul = 0
+    for i in range(N) :
+        x, a = map(int, input().split())
+        popul += a
+        XA.append((x, a))
+    XA.sort()
+    temp = 0
+    ans = 0
+    for x, a in XA :
+        temp += a
+        if temp >= popul/2 :
+           ans = x 
+           break
+    return ans
 
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     print(solution())
