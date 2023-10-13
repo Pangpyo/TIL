c2089884b69328779760a8db005e7393976e8618
김광표 <kkp0639@gmail.com>
2023-10-14 01:22:06 +0900

    1013 ps

--- Baekjoon/Bj17220.py
+++ Baekjoon/Bj17220.py
@@ -10,21 +10,13 @@ def solution() :
     reversed_graph = [[] for _ in range(26)]
     def to_num(A) :
         return ord(A) - ord('A')
+    root_list = [0]*26
     for _ in range(M) :
         a, b = map(to_num, input().split())
         graph[a].append(b)
         reversed_graph[b].append(a)
-    
-    
-    def find_root(n) :
-        parent = n
-        for nn in reversed_graph[n] :
-            parent = find_root(nn)
-        return parent
-    root_nodes = set()
-    for i in range(26) :
-        if reversed_graph[i] :
-            root_nodes.add(find_root(i))
+        root_list[b] = 1
+
     visit = [0]*26
     knows = list(input().split())
     know_list = [0]*26    
@@ -38,10 +30,10 @@ def solution() :
             dfs(nn)
 
     use = 0
-    for root_node in root_nodes :
-        if know_list[root_node] :
+    for i in range(26) :
+        if know_list[i] or root_list[i]:
             continue
-        dfs(root_node)
+        dfs(i)
         use += 1
 
     ans = sum(visit) - use
