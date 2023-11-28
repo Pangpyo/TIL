ed18273401c834f6afcd184ceaa6464086217da0
김광표 <kkp0639@gmail.com>
2023-11-28 17:55:21 +0900

    1128ps

--- Baekjoon/Bj7579.py
+++ Baekjoon/Bj7579.py
@@ -1,22 +1,29 @@
 # 7579 앱 G3
 
-n, m = map(int, input().split())
+def solution() :
 
-A = list(map(int, input().split()))
+    n, m = map(int, input().split())
 
-C = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+
+    C = list(map(int, input().split()))
 
 
-sc = sum(C)
+    sc = sum(C)
 
-D = [[0] * (n + 1) for _ in range(sc + 1)]
+    D = [[0] * (n + 1) for _ in range(sc + 1)]
+    answer = 0
+    for i in range(sc + 1):
+        for j in range(1, n + 1):
+            if i >= C[j - 1]:
+                D[i][j] = max(D[i][j - 1], D[i - C[j - 1]][j - 1] + A[j - 1])
+            else:
+                D[i][j] = D[i][j - 1]
+        if max(D[i]) >= m :
+            answer = i
+            break
+    return answer
 
-for i in range(1, sc + 1):
-    for j in range(1, n + 1):
-        if i >= C[j - 1]:
-            D[i][j] = max(D[i][j - 1], D[i - C[j - 1]][j - 1] + A[j - 1])
-        else:
-            D[i][j] = D[i][j - 1]
-    if max(D[i]) >= m:
-        print(i)
-        break
+
+if __name__ == "__main__" :
+    print(solution())
