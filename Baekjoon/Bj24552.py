5c70e82226d1a7e794dc699e6eb3300485a21e1c
김광표 <kkp0639@gmail.com>
2024-04-03 12:03:58 +0900

    0403 cs

---
+++ Baekjoon/Bj24552.py
@@ -0,0 +1,23 @@
+# 24552 올바른 괄호 G4
+
+def solution():
+    S = input()
+    sums, l, r = 0, 0, 0
+    for i, s in enumerate(S):
+        if s == '(':
+            sums += 1
+            l += 1
+        else:
+            sums -= 1
+            r += 1
+        if sums < 0 :
+            return r
+        elif sums == 0:
+            l = 0
+    return l
+
+    
+    
+
+if __name__ == "__main__":
+    print(solution())
