b8bf3d88e2e6298eec466619f1fcf7cdc0c9bdce
김광표 <kkp0639@gmail.com>
2024-04-27 23:55:55 +0900

    0427 ps

---
+++ Baekjoon/Bj19542.py
@@ -0,0 +1,37 @@
+# 19542 전단지 돌리기 G3
+
+import sys
+
+
+def solution():
+    input = sys.stdin.readline
+    N, S, D = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    to_leaf = [-1]*(N+1)
+    for _ in range(N-1):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    def get_leafs(n):
+        to_leaf[n] = 0
+        temp = 0
+        for nn in graph[n]:
+            if to_leaf[nn] != -1:
+                continue
+            temp = max(get_leafs(nn)+1, temp)
+        to_leaf[n] = temp
+        return temp
+    get_leafs(S)
+    answer = 0
+    visit = [0]*(N+1)
+    def dfs(n):
+        visit[n] = 1
+        for nn in graph[n]:
+            if visit[nn]:
+                continue
+            
+    print(visit)
+    return answer
+
+if __name__ == "__main__":
+    print(solution())
