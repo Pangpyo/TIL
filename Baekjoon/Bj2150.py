4595765bb91c278234f7b2e0706c85e6275666de
김광표 <kkp0639@gmail.com>
2024-07-01 00:37:03 +0900

    0701 ps

---
+++ Baekjoon/Bj2150.py
@@ -0,0 +1,54 @@
+# 2150 Strongly Connected Component P5
+
+import sys
+
+
+def solution():
+    input = sys.stdin.readline
+    sys.setrecursionlimit(10**6)
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
+    for _ in range(E):
+        u, v = map(int, input().split())
+        graph[u-1].append(v-1)
+    visit = [-1]*V
+    stack = []
+    on_stack = [0]*V
+    idx = 0
+    answer = []
+    def dfs(x):
+        nonlocal idx
+        idx += 1
+        stack.append(x)
+        visit[x] = idx
+        on_stack[x] = 1
+        parent = visit[x]
+        for nx in graph[x]:
+            if visit[nx] == -1:
+                parent = min(parent, dfs(nx))
+            elif on_stack[nx]:
+                parent = min(parent, visit[nx])
+        if parent == visit[x]:
+            scc = []
+            while stack:
+                node = stack.pop()
+                on_stack[node] = 0
+                scc.append(node+1)
+                if x == node:
+                    break
+            scc.sort()
+            scc.append('-1')
+            answer.append(scc)
+        return parent
+    
+    for i in range(V):
+        if visit[i] == -1:
+            dfs(i)
+    print(len(answer))
+    answer.sort()
+    for ans in answer:
+        print(*ans)
+    return answer
+
+if __name__ == "__main__":
+    solution()
